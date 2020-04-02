d2cab7e791846656c14b6ea73bfdb7770db63980
Dmitry Murzin <diralik@yandex.ru>
2020-04-02 14:24:14 +0500

    REF: Move single file

--- src/main/kotlin/org/rust/ide/refactoring/move/RsMoveFilesOrDirectoriesDialog.kt
+++ src/main/kotlin/org/rust/ide/refactoring/move/RsMoveFilesOrDirectoriesDialog.kt
@@ -92,8 +92,14 @@ class RsMoveFilesOrDirectoriesDialog(
                 runDefaultProcessor()
             }
             else -> {
-                // here our processor will run
-                runDefaultProcessor()
+                RsMoveFilesOrDirectoriesProcessor(
+                    project2,
+                    elementsToMove,
+                    targetDirectory,
+                    newParentMod,
+                    moveCallback,
+                    doneCallback
+                ).run()
             }
         }
     }
